5c8377eddf9349a0c21f6da154c39d464d120580
Mike Buckley <mbuckley@users.noreply.github.com>
2017-03-18 14:21:06 -0700

    Updated zip creator

--- tools/zip_dist.js
+++ tools/zip_dist.js
@@ -2,7 +2,7 @@ const zipFolder = require('zip-folder');
 const path = require('path');
 const packageConfig = require('../package.json');
 const DIST_PATH = path.resolve(__dirname, '..', 'dist');
-const RELEASE_PATH = path.join(DIST_PATH, 'themisui-explorer-v' + packageConfig.version + '.zip');
+const RELEASE_PATH = path.join(DIST_PATH, `${packageConfig.name}v${packageConfig.version}.zip`);
 
 zipFolder(DIST_PATH, RELEASE_PATH, function(err) {
     if(err) {
